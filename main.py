a9caca9805b34a1a18eb9e5f3d8ac4d60d8978b5
mayahNeko <mayahibela@gmail.com>
2025-09-03 09:06:25 +0300

    feat: add GET /health endpoint

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/ping")
 def get_pong():
     return Response("pong")
+
+@app.get("/health")
+def get_health():
+    return Response("Ok" , media_type= "text/plain" , status_code=200)
+
